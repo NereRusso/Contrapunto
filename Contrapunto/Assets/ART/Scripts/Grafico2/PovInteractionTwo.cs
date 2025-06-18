8060da649deec4528c5b7871750e725dfbdbf732
NereRusso <russonere01@gmail.com>
2025-06-17 21:19:23 -0300

    Audios listos y limpieza

--- Contrapunto/Assets/ART/Scripts/Grafico2/PovInteractionTwo.cs
+++ Contrapunto/Assets/ART/Scripts/Grafico2/PovInteractionTwo.cs
@@ -6,12 +6,20 @@ public class PovInteractionTwo : MonoBehaviour
     public GameObject player;
     public GameObject visorController;
 
+    [Header("Sonido al hacer click")]
+    public AudioSource audioSource;
+    public AudioClip clickSound;
+
     private bool hasBeenClicked = false;
 
     void OnMouseDown()
     {
-        if (hasBeenClicked) return;   // si ya lo clickeaste, no hace nada
+        if (hasBeenClicked) return;
         hasBeenClicked = true;
+
+        // ?? Reproducir sonido de click
+        if (audioSource && clickSound)
+            audioSource.PlayOneShot(clickSound);
 
         // Activo el modo visor
         player.SetActive(false);
@@ -19,7 +27,6 @@ public class PovInteractionTwo : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        // Opcional: desactivar este script para no procesar nada más
         this.enabled = false;
     }
 }
